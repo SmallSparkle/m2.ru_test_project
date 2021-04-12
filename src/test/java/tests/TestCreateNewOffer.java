5deb94eff6739b8c34af2d4e4f427e446d3851e9
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-04-12 13:00:54 +0200

    refactored Offer feature

--- src/test/java/tests/TestCreateNewOffer.java
+++ src/test/java/tests/TestCreateNewOffer.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 @Feature("Оффер")
 class TestCreateNewOffer extends TestBase {
@@ -26,6 +27,21 @@ class TestCreateNewOffer extends TestBase {
     $("[data-test='login-tab']").shouldHave(text("Вход"));
     $("[name='phone']").shouldBe(exist);
     $("[data-test='phone-submit-button']").shouldBe(visible);
+  }
+
+  @Test
+  @Story("Показать телефон")
+  @DisplayName("Показать телефон продавца")
+  void showPhone() {
+    step("Открываем страницу поисковой выдачи",
+            () -> open("/moskva/nedvizhimost/kupit-kvartiru/3-komnaty/?priceMax=10000000"));
+    step("Нажимаем на кнопку \"Показать телефон\"",
+            () -> $("[data-test='offers']").$("li", 0).$("[data-test='phone-button']").click());
+    step("Проверяем что на кнопке не отображается текст \"Показать телефон\"",
+            () -> $("[data-test='offers']").$("li", 0).$("[data-test='phone-button']")
+                    .$("span").shouldNotHave(text("Показать телефон")));
+
+
   }
 
 }
